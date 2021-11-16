42a0993d945ab9213307ad979e7593c975117be9
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-11-17 03:16:15 +0530

    initiated

--- android/app/src/main/java/com/movielisting/MainActivity.java
+++ android/app/src/main/java/com/movielisting/MainActivity.java
@@ -1,4 +1,4 @@
-package com.moviedetails;
+package com.movielisting;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "movieDetails";
+    return "movieListing";
   }
 }
